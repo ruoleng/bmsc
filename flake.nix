c851d869c99c5f23bf95aa2f744549454eb0565f
u2x1 <u2x1@outlook.com>
2024-12-16 13:52:47 +0800

    build: add nix flakes

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+description = "Flutter";
+inputs = {
+  nixpkgs.url = "nixpkgs/nixos-unstable";
+  flake-utils.url = "github:numtide/flake-utils";
+};
+outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
+        };
+      };
+      buildToolsVersion = "34.0.0";
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        buildToolsVersions = [ buildToolsVersion "28.0.3" ];
+        platformVersions = [ "34" "33" "28" ];
+        abiVersions = [ "arm64-v8a" ];
+      };
+      androidSdk = androidComposition.androidsdk;
+    in
+    {
+      devShell =
+        with pkgs; mkShell rec {
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          # HOME = "/home/u2x1";
+          buildInputs = [
+            flutter
+            androidSdk
+            jdk17
+            google-chrome
+            sqlite
+          ];
+          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/${buildToolsVersion}/aapt2";
+        };
+    });
+}
