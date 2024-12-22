43fbda131371380d794a3018b0636ddd89733de2
u2x1 <u2x1@outlook.com>
2024-12-22 22:16:47 +0800

    fix: scroll to current playing track on bottom sheet

--- lib/component/playlist_bottom_sheet.dart
+++ lib/component/playlist_bottom_sheet.dart
@@ -3,12 +3,34 @@ import 'package:just_audio/just_audio.dart';
 import 'package:rxdart/rxdart.dart';
 import '../cache_manager.dart';
 import '../globals.dart' as globals;
+import 'package:scroll_to_index/scroll_to_index.dart';
 
-class PlaylistBottomSheet extends StatelessWidget {
+
+class PlaylistBottomSheet extends StatefulWidget {
   const PlaylistBottomSheet({super.key});
 
   @override
+  State<PlaylistBottomSheet> createState() => _PlaylistBottomSheetState();
+}
+
+class _PlaylistBottomSheetState extends State<PlaylistBottomSheet> {
+  final AutoScrollController _scrollController = AutoScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final currentIndex = globals.api.player.currentIndex;
+      if (currentIndex != null) {
+        _scrollController.scrollToIndex(currentIndex,
+            preferPosition: AutoScrollPosition.middle);
+      }
+    });
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -142,6 +164,7 @@ class PlaylistBottomSheet extends StatelessWidget {
               }
 
               return ReorderableListView.builder(
+                scrollController: _scrollController,
                 shrinkWrap: true,
                 itemCount: playlist.length,
                 onReorder: (oldIndex, newIndex) async {
@@ -152,156 +175,150 @@ class PlaylistBottomSheet extends StatelessWidget {
                 },
                 itemBuilder: (context, index) {
                   final item = playlist[index].tag;
-                  return StreamBuilder<SequenceState?>(
-                      // Change the key to include the index to make it unique
-                      key: ValueKey('${item.id}_$index'),
-                      stream: globals.api.player.sequenceStateStream,
-                      builder: (context, snapshot) {
-                        final isPlaying = snapshot.data?.currentIndex == index;
-                        if (isPlaying) {
-                          WidgetsBinding.instance.addPostFrameCallback((_) {
-                            Scrollable.ensureVisible(
-                              context,
-                              alignment: 0.2,
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.easeInOut,
-                            );
-                          });
-                        }
-                        return ListTile(
-                          dense: true,
-                          visualDensity: const VisualDensity(vertical: -2),
-                          contentPadding:
-                              const EdgeInsets.only(left: 16, right: 8),
-                          minLeadingWidth: 24,
-                          leading: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              isPlaying
-                                  ? Icon(Icons.play_arrow,
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                      size: 20)
-                                  : Text('${index + 1}',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodySmall),
-                            ],
-                          ),
-                          title: Row(
-                            children: [
-                              if (item.extras['dummy'] ?? false)
-                                Container(
-                                  margin: const EdgeInsets.only(right: 4),
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 4, vertical: 1),
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .surfaceContainerHighest,
-                                    borderRadius: BorderRadius.circular(4),
+                  return AutoScrollTag(
+                    key: ValueKey(index),
+                    index: index,
+                    controller: _scrollController,
+                    child: StreamBuilder<SequenceState?>(
+                        key: ValueKey('${item.id}_$index'),
+                        stream: globals.api.player.sequenceStateStream,
+                        builder: (context, snapshot) {
+                          final isPlaying = snapshot.data?.currentIndex == index;
+                          return ListTile(
+                            dense: true,
+                            visualDensity: const VisualDensity(vertical: -2),
+                            contentPadding:
+                                const EdgeInsets.only(left: 16, right: 8),
+                            minLeadingWidth: 24,
+                            leading: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                isPlaying
+                                    ? Icon(Icons.play_arrow,
+                                        color:
+                                            Theme.of(context).colorScheme.primary,
+                                        size: 20)
+                                    : Text('${index + 1}',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodySmall),
+                              ],
+                            ),
+                            title: Row(
+                              children: [
+                                if (item.extras['dummy'] ?? false)
+                                  Container(
+                                    margin: const EdgeInsets.only(right: 4),
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 2, vertical: 2),
+                                    decoration: BoxDecoration(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .surfaceContainerHighest,
+                                      borderRadius: BorderRadius.circular(4),
+                                    ),
+                                    child: Icon(Icons.hourglass_empty, size: 12),
                                   ),
-                                  child: Icon(Icons.hourglass_empty, size: 12),
-                                ),
-                              Flexible(
-                                // Added Flexible widget here
-                                child: Text(
-                                  item.title,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyMedium
-                                      ?.copyWith(
-                                        color: isPlaying
-                                            ? Theme.of(context)
-                                                .colorScheme
-                                                .primary
-                                            : null,
-                                      ),
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                              if (item.extras['cached'] ?? false)
-                                const Padding(
-                                  padding: EdgeInsets.only(left: 4),
-                                  child: Icon(Icons.check_circle,
-                                      size: 16, color: Color(0xFF66BB6A)),
-                                ),
-                            ],
-                          ),
-                          subtitle: Row(
-                            children: [
-                              if (item.extras['multi'] ?? false) ...[
-                                const Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 4),
-                                  child: Icon(Icons.album, size: 12),
-                                ),
                                 Flexible(
+                                  // Added Flexible widget here
                                   child: Text(
-                                    item.extras['raw_title'] as String,
-                                    style:
-                                        Theme.of(context).textTheme.bodySmall,
+                                    item.title,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium
+                                        ?.copyWith(
+                                          color: isPlaying
+                                              ? Theme.of(context)
+                                                  .colorScheme
+                                                  .primary
+                                              : null,
+                                        ),
                                     maxLines: 1,
                                     overflow: TextOverflow.ellipsis,
                                   ),
                                 ),
-                              ] else
-                                Flexible(
-                                  child: Text(
-                                    item.artist ?? '',
-                                    style:
-                                        Theme.of(context).textTheme.bodySmall,
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
+                                if (item.extras['cached'] ?? false)
+                                  const Padding(
+                                    padding: EdgeInsets.only(left: 4),
+                                    child: Icon(Icons.check_circle,
+                                        size: 16, color: Color(0xFF66BB6A)),
                                   ),
-                                ),
-                            ],
-                          ),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              if (item.extras['multi'] ?? false)
+                              ],
+                            ),
+                            subtitle: Row(
+                              children: [
+                                if (item.extras['multi'] ?? false) ...[
+                                  const Padding(
+                                    padding: EdgeInsets.symmetric(horizontal: 4),
+                                    child: Icon(Icons.album, size: 12),
+                                  ),
+                                  Flexible(
+                                    child: Text(
+                                      item.extras['raw_title'] as String,
+                                      style:
+                                          Theme.of(context).textTheme.bodySmall,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ] else
+                                  Flexible(
+                                    child: Text(
+                                      item.artist ?? '',
+                                      style:
+                                          Theme.of(context).textTheme.bodySmall,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                              ],
+                            ),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                if (item.extras['multi'] ?? false)
+                                  Container(
+                                    margin: const EdgeInsets.only(right: 8),
+                                    child: InkWell(
+                                      onTap: () {
+                                        CacheManager.addExcludedPart(
+                                            item.extras['bvid'] as String,
+                                            item.extras['cid'] as int);
+                                        globals.api.doAndSave(() async {
+                                          await globals.api.playlist
+                                              .removeAt(index);
+                                        });
+                                      },
+                                      child: const Icon(Icons.not_interested,
+                                          size: 20),
+                                    ),
+                                  ),
                                 Container(
                                   margin: const EdgeInsets.only(right: 8),
                                   child: InkWell(
                                     onTap: () {
-                                      CacheManager.addExcludedPart(
-                                          item.extras['bvid'] as String,
-                                          item.extras['cid'] as int);
                                       globals.api.doAndSave(() async {
                                         await globals.api.playlist
                                             .removeAt(index);
                                       });
                                     },
-                                    child: const Icon(Icons.not_interested,
-                                        size: 20),
+                                    child: const Icon(Icons.delete, size: 20),
                                   ),
                                 ),
-                              Container(
-                                margin: const EdgeInsets.only(right: 8),
-                                child: InkWell(
-                                  onTap: () {
-                                    globals.api.doAndSave(() async {
-                                      await globals.api.playlist
-                                          .removeAt(index);
-                                    });
-                                  },
-                                  child: const Icon(Icons.delete, size: 20),
+                                ReorderableDragStartListener(
+                                  index: index,
+                                  child: const Icon(Icons.drag_handle, size: 24),
                                 ),
-                              ),
-                              ReorderableDragStartListener(
-                                index: index,
-                                child: const Icon(Icons.drag_handle, size: 24),
-                              ),
-                            ],
-                          ),
-                          onTap: () async {
-                            await globals.api.player
-                                .seek(Duration.zero, index: index);
-                            await globals.api.player.play();
-                          },
-                        );
-                      });
+                              ],
+                            ),
+                            onTap: () async {
+                              await globals.api.player
+                                  .seek(Duration.zero, index: index);
+                              await globals.api.player.play();
+                            },
+                          );
+                        }),
+                  );
                 },
               );
             },
